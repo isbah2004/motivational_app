43ca0de19bfd2247ccbec975f47352faca6dcc98
Internee Desk <Deski02@lotte-ppta.com>
2024-07-22 15:49:43 +0500

    22/7/2024

--- lib/homescreen/hometabs/philosphy_tab.dart
+++ lib/homescreen/hometabs/philosphy_tab.dart
@@ -1,9 +1,39 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-class PhilosphyTab extends StatelessWidget {
+
+class PhilosphyTab extends StatefulWidget {
   const PhilosphyTab({super.key});
 
   @override
+  State<PhilosphyTab> createState() => _PhilosphyTabState();
+}
+
+class _PhilosphyTabState extends State<PhilosphyTab> {
+ 
+
+  final fireStore = FirebaseFirestore.instance.collection('users');
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+      body: const Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          String id = DateTime.now().millisecondsSinceEpoch.toString();
+          fireStore.doc(id).set({
+            'full_name': "asdf", // John Doe
+            'company': "adsf", // Stokes and Sons
+            'age': 12,
+            'id': id
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+
   }
 }
--- lib/homescreen/hometabs/poems_tab.dart
+++ lib/homescreen/hometabs/poems_tab.dart
@@ -1,9 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-class PoemTab extends StatelessWidget {
+
+class PoemTab extends StatefulWidget {
   const PoemTab({super.key});
 
   @override
+  State<PoemTab> createState() => _PoemTabState();
+}
+
+class _PoemTabState extends State<PoemTab> {
+  final fireStore = FirebaseFirestore.instance.collection('users').snapshots();
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: fireStore,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Loading");
+              }
+
+              return Expanded(
+                child: ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['full_name'].toString()),
+                      subtitle: Text(data['company']),
+                      onTap: () {
+                        users
+                            .doc(data['id'].toString())
+                            .delete()
+                            .then((value) => debugPrint("User Updated"))
+                            .catchError((error) =>
+                                debugPrint("Failed to update user: $error"));
+                        users
+                            .doc(data['id'].toString())
+                            .update({'full_name': 'Asif Taj'})
+                            .then((value) => debugPrint("User Updated"))
+                            .catchError((error) =>
+                                debugPrint("Failed to update user: $error"));
+                      },
+                    );
+                  }).toList(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
